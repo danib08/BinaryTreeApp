dbfea46a828df9c3d710c89705779e033ac8e926
Jean Franco Gondrez <jeanfranco140299@gmail.com>
2020-07-26 18:54:28 -0600

    Added-comments
    Added comments to the BSTNode and BinarySearchTree classes

--- BinaryTreeApp/BinaryTreeApp/BSTNode.cs
+++ BinaryTreeApp/BinaryTreeApp/BSTNode.cs
@@ -1,11 +1,23 @@
 ﻿namespace BinaryTreeApp
 {
+    /// <summary>
+    /// This class is the Node for the Binary search Tree
+    /// </summary>
     public class BSTNode
-    {
+    { 
+        /// <summary>
+        /// int Element contained by the node uses properties
+        /// </summary>
         public int Element { get; set; }
 
+        /// <summary>
+        /// BSTNode Right right child of the node uses properties
+        /// </summary>
         public BSTNode Right { get; set; }
 
+        /// <summary>
+        /// BSTNode Left left child of the node uses properties
+        /// </summary>
         public BSTNode Left { get; set; }
     }
 }
--- BinaryTreeApp/BinaryTreeApp/BinarySearchTree.cs
+++ BinaryTreeApp/BinaryTreeApp/BinarySearchTree.cs
@@ -2,21 +2,38 @@
 
 namespace BinaryTreeApp
 {
+    /// <summary>
+    /// This class is the BinarySearchTree data structure
+    /// </summary>
     class BinarySearchTree
     {
+        /// <summary>
+        /// BSTNode Root Node being the root of the tree uses properties
+        /// </summary>
         private BSTNode Root { get; set; }
 
+        
         private List<int> preOrderList = new List<int>();
-
+        
         private List<int> inOrderList  = new List<int>();
-
+        
         private List<int> postOrderList = new List<int>();
 
+        /// <summary>
+        /// Adds a new element to the tree. Calls private method insert.
+        /// </summary>
+        /// <param name="element"> int the element to be added to the tree </param>
         public void insert(int element)
         {
             Root = insert(element, Root);
         }
-
+        
+        /// <summary>
+        /// Adds a new element to the tree. Calls itself recursively.
+        /// </summary>
+        /// <param name="element"> int the element to be added to the tree </param>
+        /// <param name="current"> BSTNode the current node being visited </param>
+        /// <returns></returns>
         private BSTNode insert(int element, BSTNode current)
         {
             if (current == null)
@@ -38,13 +55,22 @@ namespace BinaryTreeApp
 
             return current;
         }
-
+        
+        /// <summary>
+        /// Gets a list of the tree using the preOrder traversal. Calls private method printPreOrder
+        /// </summary>
+        /// <returns> List<int> list with the elements in preOrder </returns>
         public List<int> printPreOrder()
         {
             preOrderList.Clear();
             return printPreOrder(Root);
         }
-
+        
+        /// <summary>
+        /// Gets a list of the tree using the preOrder traversal. Calls itself recursively
+        /// </summary>
+        /// <param name="current"> BSTNode current node is being visited </param>
+        /// <returns> List<int> list with the elements in preOrder </returns>
         private List<int> printPreOrder(BSTNode current)
         {
             if (current == null) return preOrderList;
@@ -53,13 +79,22 @@ namespace BinaryTreeApp
             printPreOrder(current.Right);
             return preOrderList;
         }
-
+        
+        /// <summary>
+        /// Gets a list of the tree using the inOrder traversal. Calls private method printInOrder
+        /// </summary>
+        /// <returns> List<int> list with the elements in inOrder </returns>
         public List<int> printInOrder()
         {
             inOrderList.Clear();
             return printInOrder(Root);
         }
-
+        
+        /// <summary>
+        /// Gets a list of the tree using the inOrder traversal. Calls itself recursively
+        /// </summary>
+        /// <param name="current"> BSTNode current node is being visited </param>
+        /// <returns> List<int> list with the elements in inOrder </returns>
         private List<int> printInOrder(BSTNode current)
         {
             if (current == null) return inOrderList;
@@ -69,12 +104,21 @@ namespace BinaryTreeApp
             return inOrderList;
         }
 
+        /// <summary>
+        /// Gets a list of the tree using the postOrder traversal. Calls private method printPostOrder
+        /// </summary>
+        /// <returns> List<int> list with the elements in postOrder </returns>
         public List<int> printPostOrder()
         {
             postOrderList.Clear();
             return printPostOrder(Root);
         }
-
+        
+        /// <summary>
+        /// Gets a list of the tree using the postOrder traversal. Calls itself recursively
+        /// </summary>
+        /// <param name="current"> BSTNode current node is being visited </param>
+        /// <returns> List<int> list with the elements in postOrder </returns>
         private List<int> printPostOrder(BSTNode current)
         {
             if (current == null) return postOrderList;
